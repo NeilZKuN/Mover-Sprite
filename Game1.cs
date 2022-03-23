2d63a766d9a202bf28e83dc5d7e366abf0201906
NeilZKuN <101401816+NeilZKuN@users.noreply.github.com>
2022-03-23 22:51:48 +0000

    Add files via upload

---
+++ Game1.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace KeyBoard_Manager_Project
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Vector2 _position;
+        private KeyBoardManager km;
+        Texture2D t2d;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            km = new KeyBoardManager();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _position = new Vector2(0, 0);
+            t2d = Content.Load<Texture2D>("hqdefault");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            /*if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();*/
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                _position.Y -= 1;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                _position.X -= 1;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                _position.Y += 1;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                _position.X += 1;
+            }
+
+            // TODO: Add your update logic here
+            km.Update();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(t2d, _position, Color.White);
+            _spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ KeyBoardManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace KeyBoard_Manager_Project
+{
+    class KeyBoardManager
+    {
+        private static KeyBoardManager instance;
+        enum KeyState 
+        {
+            PRESSED,
+            HELD,
+            UP,
+            NONE
+        }
+
+        Dictionary<Keys, KeyState> keysandState;
+
+        public KeyBoardManager() 
+        {
+            if (instance == null)
+            {
+                keysandState = new Dictionary<Keys, KeyState>();
+                instance = this;
+            }
+            else
+            {
+                throw new Exception("Instance already created");
+            }
+
+
+            keysandState = new Dictionary<Keys, KeyState>();
+        }
+
+        public void Update() 
+        {
+            KeyboardState state = Keyboard.GetState();
+            Keys[] pressedKeys = state.GetPressedKeys();
+
+            foreach (Keys k in pressedKeys)
+            {
+                if (!keysandState.ContainsKey(k))
+                {
+                    keysandState.Add(k, KeyState.PRESSED);
+                }
+                else
+                {
+                    if (keysandState[k] == KeyState.PRESSED)
+                    {
+                        keysandState[k] = KeyState.HELD;
+                    }
+                }
+            }
+
+            foreach (Keys k in keysandState.Keys.ToArray())
+            {
+                if (!pressedKeys.Contains(k))
+                {
+                    if (keysandState[k] == KeyState.UP)
+                    {
+                        keysandState[k] = KeyState.NONE;
+                    }
+                    else if (keysandState[k] == KeyState.PRESSED || keysandState[k] == KeyState.HELD)
+                    {
+                        keysandState[k] = KeyState.UP;
+                    }
+                }
+            }
+        }
+
+        bool IsKeyPressed(Keys k) => keysandState.ContainsKey(k) && keysandState[k] == KeyState.PRESSED;
+        bool ISKeyUp(Keys k) => keysandState.ContainsKey(k) && keysandState[k] == KeyState.UP;
+        bool ISKeyHeld(Keys k) => keysandState.ContainsKey(k) && keysandState[k] == KeyState.HELD;
+    }
+}
